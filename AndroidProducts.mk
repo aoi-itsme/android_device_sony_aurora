341b99e7d63922f1bc8517921ec23ec205e83d72
aoitsme <kanonify01@gmail.com>
2026-05-23 21:11:33 +0700

    aurora: Bringup InfinityX vanilla

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_aurora.mk
+    $(LOCAL_DIR)/infinity_aurora.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_aurora-user \
-    lineage_aurora-userdebug \
-    lineage_aurora-eng
+    infinity_aurora-user \
+    infinity_aurora-userdebug \
+    infinity_aurora-eng
--- infinity_aurora.mk
+++ infinity_aurora.mk
@@ -8,20 +8,25 @@
 $(call inherit-product, device/sony/aurora/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Setup keystore
 -include vendor/lineage-priv/keys/keys.mk
 
-PRODUCT_NAME := lineage_aurora
+PRODUCT_NAME := infinity_aurora
 PRODUCT_DEVICE := aurora
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Xperia XZ2 Premium
 
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_BLUR := true
 TARGET_ENABLE_BLUR := true
 TARGET_DISABLE_EPPE := true
+
+INFINITY_MAINTAINER := Aoitsme
+WITH_GAPPS := false
+PRODUCT_NO_CAMERA := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-sony-mobile
 
